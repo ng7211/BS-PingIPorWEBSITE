25048987bff1e49a49605c8287ef6b014e884fbc
ng7211 <demy.abdulsalami@haw-hamburg.de>
2025-10-31 04:00:45 +0100

    Add files via upload
    Python-Skript try_host.py [-h|-s <sec>] <hostname>|<IP-Address>
    Anforderungen:
    • Das Skript soll einen Hostnamen oder eine IP-Adresse als Befehlszeilenargument entgegennehmen.
    • Das Skript soll die Option -h unterstützen, die eine Hilfenachricht ausgibt.
    • Das Skript soll die Option -s <sec> unterstützen, die das Intervall zwischen den Pings in Sekunden angibt. Standardmäßig soll das Intervall 3 Sekunden betragen.
    • Das Skript soll den Host in regelmäßigen Abständen pingen und den Host sowie Status (OK
    oder FAILED) ausgeben.
    • Das Skript soll eine Fehlermeldung ausgeben, wenn keine gültigen Argumente übergeben werden.
    Das Skript unterstützt folgende Optionen (es darf aber nur eine Option gleichzeitig angegeben
    werden):
    • -h : Nur Ausgabe der „Usage Message"
    • -s <sec> : Der ping wird zyklisch alle <sec> Sekunden ausgeführt.
    Fehlt die -s Option, wird der ping alle 3 Sekunden ausgeführt.
    Beispiel: Der Aufruf
    python3 t r y _hos t . py −s 5 google . de
    erzeugt alle 5 Sekunden eine Ausgabe der Art:
    google . de OK
    falls der Host google.de erreichbar ist, anderenfalls
    google . de FAILED

---
+++ try_host.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import time
+
+#Parseer-Objekt mit Deskription erstellen.
+parser = argparse.ArgumentParser(description="This script pings websites or IP adresses in intervalls and shows status OK/FAILED")
+#Optionales Argument 
+parser.add_argument('-s', '--seconds', type=float, default=3.0, help='This options sets the time in seconds between Pings.')
+#Positinales Argument
+parser.add_argument("website", help="The website or IP adress to ping.")
+#Verarbeitet die Argument die über dir Befehlszeile übergeben wurden
+args = parser.parse_args()
+
+
+
+def pingIpOrWebsite(seconds, ipOrWebsite):
+    while True:
+        #Einen Ping senden und danach beenden
+        response = os.popen(f"ping -c 1 {ipOrWebsite}").read()
+        #Jede erfolgreiche Ping Nachricht hat in Linux ein ttl -> time to live, also Host ist erraichbar
+        if "ttl=" in response.lower():
+            print(f"{ipOrWebsite} OK")
+        else:
+            print(f"{ipOrWebsite} FAILED")
+        #Das Zeitintervall
+        time.sleep(seconds)
+
+if __name__ == '__main__':
+    pingIpOrWebsite(args.seconds, args.website)
+
